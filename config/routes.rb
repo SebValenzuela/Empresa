c59afc0b5802824df5621a6a9fd13e9e7638173f
Sebastián <seba.val.garat@gmail.com>
2019-06-19 21:18:20 -0400

    entre commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resourcer :companies
+  resourcer :employees, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'companies#index'
+root 'employees#companies'
+
+get '/employees/companies', to: 'employees#companies'
 end
